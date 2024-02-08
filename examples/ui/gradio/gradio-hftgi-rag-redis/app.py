eb750b020d2a8f99ac73b2f2c50da5cc4f0f7f21
sauagarwa <sauagarw@redhat.com>
2024-02-08 09:36:31 -0500

    Add provider base implementation

--- examples/ui/gradio/gradio-hftgi-rag-redis/app.py
+++ examples/ui/gradio/gradio-hftgi-rag-redis/app.py
@@ -1,45 +1,44 @@
 import os
-import random
 import time
 from collections.abc import Generator
-from queue import Empty, Queue
+from queue import Empty
 from threading import Thread
-from typing import Optional
 import os
 from markdown import markdown
+from llm.llm_factory import LLMFactory
 import pdfkit
 import uuid
 import threading
 import gradio as gr
-from prometheus_client import start_http_server, Counter
-import tempfile
+from prometheus_client import Gauge, start_http_server, Counter
 from dotenv import load_dotenv
-from langchain.callbacks.base import BaseCallbackHandler
-from langchain.chains import RetrievalQA
-from langchain.embeddings.huggingface import HuggingFaceEmbeddings
-from langchain.llms import HuggingFaceTextGenInference
-from langchain.prompts import PromptTemplate
-from langchain.vectorstores.redis import Redis
-from prometheus_client import start_http_server, Counter, Histogram, Gauge
-import requests
+from utils import config_loader
+import llm.query_helper as QueryHelper
+from scheduler.round_robin import RoundRobinScheduler
 
+# test data to schedule
+# data = [ ("A", 6), 
+#         ("B", 5), 
+#         ("C", 4), 
+#         ("D", 3), 
+#         ("E", 2),
+#         ("F", 1),
+#       ]
+
+
+# initialization
 load_dotenv()
+config_loader.init_config()
+llm_factory = LLMFactory()
+llm_factory.init_providers(config_loader.config)
+provider_model_weightage_list = config_loader.get_provider_model_weightage_list()
+# initialize scheduler
+sched = RoundRobinScheduler(provider_model_weightage_list)
+
 
 # Parameters
-
 APP_TITLE = os.getenv('APP_TITLE', 'Talk with your documentation')
-
-INFERENCE_SERVER_URL = os.getenv('INFERENCE_SERVER_URL')
-MAX_NEW_TOKENS = int(os.getenv('MAX_NEW_TOKENS', 512))
-TOP_K = int(os.getenv('TOP_K', 10))
-TOP_P = float(os.getenv('TOP_P', 0.95))
-TYPICAL_P = float(os.getenv('TYPICAL_P', 0.95))
-TEMPERATURE = float(os.getenv('TEMPERATURE', 0.01))
-REPETITION_PENALTY = float(os.getenv('REPETITION_PENALTY', 1.03))
 PDF_FILE_DIR = "proposal-docs"
-
-REDIS_URL = os.getenv('REDIS_URL')
-REDIS_INDEX = os.getenv('REDIS_INDEX')
 TIMEOUT = int(os.getenv('TIMEOUT', 30))
 
 # Start Prometheus metrics server
@@ -49,44 +48,24 @@ start_http_server(8000)
 FEEDBACK_COUNTER = Counter("feedback_stars", "Number of feedbacks by stars", ["stars", "model_id"])
 MODEL_USAGE_COUNTER = Counter('model_usage', 'Number of times a model was used', ['model_id'])
 REQUEST_TIME = Gauge('request_duration_seconds', 'Time spent processing a request', ['model_id'])
-        
-def get_model_id():
-    model_id = "Unavailable"
-    try:
-        r = requests.get(f'{INFERENCE_SERVER_URL}/info')
-        if r.status_code == 200:
-            model_id = r.json()['model_id']
-    except TimeoutError:  # or whatever exception your client throws on timeout
-            end_time = time.perf_counter()
-    return model_id
 
-model_id = get_model_id()
+
 # PDF Generation
 def get_pdf_file(session_id):
     return os.path.join("./assets", PDF_FILE_DIR, f"proposal-{session_id}.pdf")
 
 def create_pdf(text, session_id):
-    output_filename = get_pdf_file(session_id)
-    html_text = markdown(text, output_format='html4')
-    pdf=pdfkit.from_string(html_text, output_filename)
+    try:
+        output_filename = get_pdf_file(session_id)
+        html_text = markdown(text, output_format='html4')
+        pdfkit.from_string(html_text, output_filename)
+    except Exception as e:
+        print(e)
 
 # Function to initialize all star ratings to 0
 def initialize_feedback_counters(model_id):
     for star in range(1, 6):  # For star ratings 1 to 5
         FEEDBACK_COUNTER.labels(stars=str(star), model_id=model_id).inc(0)
-
-# Streaming implementation
-class QueueCallback(BaseCallbackHandler):
-    """Callback handler for streaming LLM responses to a queue."""
-
-    def __init__(self, q):
-        self.q = q
-
-    def on_llm_new_token(self, token: str, **kwargs: any) -> None:
-        self.q.put(token)
-
-    def on_llm_end(self, *args, **kwargs: any) -> None:
-        return self.q.empty()
 
 def remove_source_duplicates(input_list):
     unique_list = []
@@ -97,9 +76,11 @@ def remove_source_duplicates(input_list):
 
 lock = threading.Lock()
 
-def stream(input_text, session_id) -> Generator:
+def stream(llm, q, input_text, session_id, model_id) -> Generator:
     # Create a Queue
     job_done = object()
+
+    qa_chain = QueryHelper.get_qa_chain(llm)
 
     # Create a function to call - this will run in a thread
     def task():
@@ -108,17 +89,21 @@ def stream(input_text, session_id) -> Generator:
         initialize_feedback_counters(model_id)
         with lock:
             start_time = time.perf_counter() # start and end time to get the precise timing of the request
-            resp = qa_chain({"query": input_text})
-            end_time = time.perf_counter()
-            sources = remove_source_duplicates(resp['source_documents'])
-            # Record successful request time
-            REQUEST_TIME.labels(model_id=model_id).set(end_time - start_time)
-        create_pdf(resp['result'], session_id)
-        if len(sources) != 0:
-            q.put("\n*Sources:* \n")
-            for source in sources:
-                q.put("* " + str(source) + "\n")
-        q.put(job_done)
+            try:
+                resp = qa_chain({"query": input_text})
+                end_time = time.perf_counter()
+                sources = remove_source_duplicates(resp['source_documents'])
+                REQUEST_TIME.labels(model_id=model_id).set(end_time - start_time)
+                create_pdf(resp['result'], session_id)
+                if len(sources) != 0:
+                    q.put("\n*Sources:* \n")
+                    for source in sources:
+                        q.put("* " + str(source) + "\n")
+            except Exception as e:
+                print(e)
+                q.put("Error executing request. Contact the administrator.")
+
+            q.put(job_done)
 
     # Create a thread and start the function
     t = Thread(target=task)
@@ -138,93 +123,47 @@ def stream(input_text, session_id) -> Generator:
         except Empty:
             continue
 
-# A Queue is needed for Streaming implementation
-q = Queue()
-
-############################
-# LLM chain implementation #
-############################
-
-# Document store: Redis vector store
-embeddings = HuggingFaceEmbeddings()
-rds = Redis.from_existing_index(
-    embeddings,
-    redis_url=REDIS_URL,
-    index_name=REDIS_INDEX,
-    schema="redis_schema.yaml"
-)
-
-# LLM
-llm = HuggingFaceTextGenInference(
-    inference_server_url=INFERENCE_SERVER_URL,
-    max_new_tokens=MAX_NEW_TOKENS,
-    top_k=TOP_K,
-    top_p=TOP_P,
-    typical_p=TYPICAL_P,
-    temperature=TEMPERATURE,
-    repetition_penalty=REPETITION_PENALTY,
-    streaming=True,
-    verbose=False,
-    callbacks=[QueueCallback(q)]
-)
-
-
-prompt_template="""<s>[INST] <<SYS>>
-Generate project proposal for the product owned by Red Hat.
-You will be given a product and the customer information, and a context to provide you with information. 
-You must generate the proposal based as much as possible on this context.
-
-Proposal should be addressed to the customer and should be from the company owning the product.
-
-The proposal should include an overview of the product restricted to three items, its features and benefits restricted to three items.
-The proposal should also include pricing strategy.
-
-The proposal should contain headings and sub-headings and each heading and sub-heading should be in bold. 
-Each section should contain only three items.
-
-Proposal should be minimum of 500 lines.
-<</SYS>>
-
-Question: {question}
-Context: {context} [/INST]
-"""
-
-QA_CHAIN_PROMPT = PromptTemplate(
-    template=prompt_template,
-    input_variables=["context", "question"])
-
-qa_chain = RetrievalQA.from_chain_type(
-    llm,
-    retriever=rds.as_retriever(
-        search_type="similarity",
-        search_kwargs={"k": 4, "distance_threshold": 0.5}),
-    chain_type_kwargs={"prompt": QA_CHAIN_PROMPT},
-    return_source_documents=True
-    )
-
-# qa_chain = RetrievalQA.from_chain_type(
-#     llm,
-#     chain_type='stuff',
-#     retriever=rds.as_retriever(),
-#     return_source_documents=True,
-#     chain_type_kwargs={"prompt": QA_CHAIN_PROMPT}
-# )
-
 # Gradio implementation
-def ask_llm(customer, product):
+def ask_llm(provider_model, customer, product):
     session_id = str(uuid.uuid4())
+    provider_id, model_id = get_provider_model(provider_model)
+    llm, q = llm_factory.get_llm(provider_id, model_id)
+
     query = f"Generate a Sales Proposal for the product '{product}' to sell to company '{customer}' that includes overview, features, benefits, and support options?"
-    for next_token, content in stream(query, session_id):
+    for next_token, content in stream(llm, q, query, session_id, model_id):
         # Generate the download link HTML
         download_link_html = f' <input type="hidden" id="pdf_file" name="pdf_file" value="/file={get_pdf_file(session_id)}" />'
-        yield content, download_link_html    
+        yield content, download_link_html
 
+def get_provider_model(provider_model):
+    if provider_model is None:
+        return "",""
+    s = provider_model.split(": ")
+    return s[0], s[1]
 
+def is_provider_visible():
+    return config_loader.config.type == "all"
+
+def get_selected_provider():
+    if config_loader.config.type == "round_robin":
+        return sched.get_next()
+    
+    provider_list = config_loader.get_provider_model_weightage_list()
+    if len(provider_list) > 0:
+        return provider_list[0]
+    
+    return None
+        
 # Gradio implementation
 css = "#output-container {font-size:0.8rem !important}"
 with gr.Blocks(title="HatBot") as demo:
+    provider_model_list = config_loader.get_provider_model_list()
+    provider_model_var = gr.State()
+    provider_visible = is_provider_visible()
+
     with gr.Row():
         with gr.Column(scale=1):
+            providers_dropdown = gr.Dropdown(label="Providers", choices=provider_model_list)  
             customer_box = gr.Textbox(label="Customer", info="Enter the customer name")
             product_dropdown = gr.Dropdown(
              ["Red Hat OpenShift", "Red Hat OpenShift Data Science", "Red Hat AMQ Streams"], label="Product", info="Select the product to generate proposal"
@@ -232,29 +171,66 @@ with gr.Blocks(title="HatBot") as demo:
             with gr.Row():
                 submit_button = gr.Button("Generate")
                 clear_button = gr.LogoutButton(value="Clear", icon=None)
+            model_text = gr.HTML(visible=~provider_visible)
 
-            gr.HTML(f"<div><span id='model_id'>Model: {model_id}</span></div>")
+            def update_models(selected_provider, provider_model):
+                provider_id, model_id = get_provider_model(selected_provider)
+                m=f"<div><span id='model_id'>Model: {model_id}</span></div>"
+                return {
+                    provider_model_var: selected_provider,
+                    model_text: m
+                }
+            
+            providers_dropdown.input(update_models, inputs=[providers_dropdown, provider_model_var], outputs=[provider_model_var,model_text])
             radio = gr.Radio(["1", "2", "3", "4", "5"], label="Rate the model")
             output_rating = gr.Textbox(elem_id="source-container", interactive=True, label="Rating")
 
         with gr.Column(scale=2):
-            output_answer = gr.Textbox(label="Project Proposal", interactive=True, lines=19, elem_id="output-container", scale=4, max_lines=19)
-            # path = gr.Textbox(label="PDF file", interactive=True, lines=2, elem_id="output-container", scale=4, max_lines=3)
+            lines = 19
+            if provider_visible:
+                lines = 26
+            output_answer = gr.Textbox(label="Project Proposal", interactive=True, lines=lines, elem_id="output-container", scale=4, max_lines=lines)
             download_button = gr.Button("Download as PDF")
             download_link_html=gr.HTML(visible=False)
 
     download_button.click(None, [], [], js="() => window.open(document.getElementById('pdf_file').value, '_blank')")
-    submit_button.click(ask_llm, inputs=[customer_box, product_dropdown], outputs=[output_answer,download_link_html])
+    submit_button.click(ask_llm, inputs=[providers_dropdown, 
+                                         customer_box, 
+                                         product_dropdown], 
+                                outputs=[output_answer,
+                                         download_link_html])
     clear_button.click(lambda: [None, None ,None , None, None], 
                        inputs=[], 
-                       outputs=[customer_box,product_dropdown,output_answer,radio,output_rating])
+                       outputs=[customer_box,
+                                product_dropdown,
+                                output_answer,
+                                radio,
+                                output_rating])
 
-    @radio.input(inputs=radio, outputs=output_rating)
-    def get_feedback(star):
-        print("Rating: " + star)
+    @radio.input(inputs=[radio, provider_model_var], outputs=output_rating)
+    def get_feedback(star, provider_model):
+        provider_id, model_id = get_provider_model(provider_model)
+        print(f"Model: {provider_model}, Rating: {star}")
         # Increment the counter based on the star rating received
         FEEDBACK_COUNTER.labels(stars=str(star), model_id=model_id).inc()
         return f"Received {star} star feedback. Thank you!"
+
+    def initialize(provider_model):
+        if provider_model is None:
+            provider_model_tuple = get_selected_provider()
+            if provider_model_tuple is not None:
+                provider_model = provider_model_tuple[0]
+        print(provider_model)
+        provider_id, model_id = get_provider_model(provider_model)
+        provider_visible = is_provider_visible()
+        p_dropdown = gr.Dropdown(choices=provider_model_list, label="Providers", visible=provider_visible, value=provider_model)
+        m=f"<div><span id='model_id'>Model: {model_id}</span></div>"
+        return {
+            providers_dropdown: p_dropdown,
+            provider_model_var: provider_model,
+            model_text: m
+        }
+    demo.load(initialize, inputs=[provider_model_var], outputs=[providers_dropdown,provider_model_var,model_text])
 
 if __name__ == "__main__":
     demo.queue().launch(
